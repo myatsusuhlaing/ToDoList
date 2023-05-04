6c293e145c6ac4155e5fa1a8c6993da7eff03add
myatsusuhlaing <myatsusuhlaing04@gmail.com>
2023-05-04 10:05:47 +0630

    text line break list

--- src/App.js
+++ src/App.js
@@ -32,11 +32,11 @@ function TodoList(){
   };
   return(
   <>
-  <div className='bg-slate-300 md:container md:mx-auto text-center m-5 mb-10  h-auto drop-shadow-[0_35px_35px_rgba(0,0,0,.35)]'>
-    <div className="py-4 md:py-8 md:mx-auto">
-      <h2 className='p-4 font-mono text-xl italic md:not-italic'>To-Do List</h2>
+  <div  className='bg-slate-300 md:container md:mx-auto text-center m-5 mb-10 pb-10 h-auto drop-shadow-[0_35px_35px_rgba(0,0,0,.35)]'>
+    <div className="py-4 md:py-8 md:m-auto">
+      <h2 className='p-4 font-mono text-2xl italic md:not-italic'>To-Do List</h2>
       <input 
-      className='p-2 w-6/12'
+      className='p-2 w-8/12 placeholder:italic placeholder:text-slate-400'
       type='text' 
       placeholder='Note Something.......'
       value={addlist}
@@ -48,24 +48,25 @@ function TodoList(){
         <AiOutlinePlus size={15} />
       </button>
     </div>
-    <div className='checkList border border-slate-300 w-7/12 text-left p-5 mx-auto'>
+    <div className='checkList border border-slate-300 w-9/12 text-left p-5 mx-auto '>
         <ul >
           {notes.map((note,index) =>
             <li key={index} 
             style={{textDecoration: note.completed ? "line-through" : "none"}}
-            className='text-sm font-semibold bg-blue-300 flex h-16 w-full items-center rounded-md bg-primary p-4 mb-2'
+            className='text-sm font-semibold bg-blue-300 flex h-aut0 w-full items-center rounded-md bg-primary p-4 mb-1'
             >
             <input 
               type='checkbox' 
               checked ={note.completed}
               onChange={()=>toggleNew(index)}
+              className='mb-auto'
             />
-            <span className='p-1'>
+            <p className='pl-2 overflow-hidden break-words leading-5 '>
               {note.label}
-            </span>
+            </p>
           {/* <button onClick={() => deleteList(index)}>delete</button>  */}
           <button onClick={deleteBtn} className='deltebtn rounded inline-block bg-slate-800 px-3 pb-3.5 pt-3 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800  focus:bg-neutral-800]' >
-            <AiFillDelete size={15} />
+            <AiFillDelete size={15} className='group-hover:stroke-white'/>
           </button>
           </li>
           )}
